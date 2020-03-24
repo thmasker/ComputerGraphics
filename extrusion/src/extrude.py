ac6f0fa9479b11abf1c756e7e61d3b6e8fd5e88b
thmasker <diego.p.hidalgo@gmail.com>
2020-03-24 18:40:45 +0100

    Extrusion

--- extrusion/src/extrude.py
+++ extrusion/src/extrude.py
@@ -8,108 +8,109 @@ import utils
 
 
 def usage():
-	print('Simple Extrusion')
-	print('------------------------------------')
-	print('Usage: extrude <options> <.obj file>*')
-	print('<options>:')
-	print('\t-d, --distance=')
-	print('------------------------------------')
-	print('IMPORTANT: Input files MUST define triangulated faces')
+    print('Simple Extrusion')
+    print('------------------------------------')
+    print('Usage: extrude <options> <.obj file>*')
+    print('<options>:')
+    print('\t-d, --distance=')
+    print('------------------------------------')
+    print('IMPORTANT: Input files MUST define triangulated faces')
+
 
 def main(argv: List[str]):
-	distance = 0
-	try:
-		opts, args = getopt.getopt(argv, "d:", ['help', 'distance='])
-	except getopt.GetoptError:
-		usage()
-		sys.exit(2)
+    distance = 0
+    try:
+        opts, args = getopt.getopt(argv, "d:", ['help', 'distance='])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
-	for opt, arg in opts:
-		if opt == '--help':
-			usage()
-			sys.exit()
-		elif opt in ('-d', '--distance'):
-			try:
-				distance = float(arg)
-			except ValueError:
-				print("Distance '" + arg +  "' is not a valid number (int or float)")
-				sys.exit(2)
+    for opt, arg in opts:
+        if opt == '--help':
+            usage()
+            sys.exit()
+        elif opt in ('-d', '--distance'):
+            try:
+                distance = float(arg)
+            except ValueError:
+                print("Distance '" + arg +  "' is not a valid number (int or float)")
+                sys.exit(2)
 
-	for file in args:
-		try:
-			f = open(file)
-		except FileNotFoundError as fne:
-			print(fne.strerror + ": '" + file + "'")
-			sys.exit(2)
+    for file in args:
+        try:
+            f = open(file)
+        except FileNotFoundError as fne:
+            print(fne.strerror + ": '" + file + "'")
+            sys.exit(2)
 
-		lines = f.readlines()
-		f.close()
+        lines = f.readlines()
+        f.close()
 
-		vertices = []
-		faces = []
-		info = []
-		for line in lines:
-			if line[0] == 'v':
-				vertices.append(utils.toVertex(line))
-			elif line[0] == 'f':
-				faces.append(utils.toFace(line))
-			else:
-				info.append(line)
+        vertices = []
+        faces = []
+        info = []
+        for line in lines:
+            if line[0] == 'v':
+                vertices.append(utils.toVertex(line))
+            elif line[0] == 'f':
+                faces.append(utils.toFace(line))
+            else:
+                info.append(line)
 
-		n_vertices = len(vertices)
-		n_faces = len(faces)
+        n_vertices = len(vertices)
+        n_faces = len(faces)
 
-		'''
-		Step 1
-		For each vertex, add distance * normalVector to it, appending result to the vertex array
-		'''
-		normal = utils.normalVector(vertices, faces)
-		normal = [n * distance for n in normal]
-		for i in range(n_vertices):
-			vertices.append([v + n_i for (v, n_i) in zip(vertices[i], normal)])
-		
-		'''
-		Step 2
-		Create list of boundary edges
-		'''
-		combinations = []
-		for face in faces:
-			combinations.append([(face[0], face[1]), (face[1], face[2]), (face[2], face[0])])
+        '''
+        Step 1
+        For each vertex, add distance * normalVector to it, appending result to the vertex array
+        '''
+        normal = utils.normalVector(vertices, faces)
+        normal = [n * distance for n in normal]
+        for i in range(n_vertices):
+            vertices.append([v + n_i for (v, n_i) in zip(vertices[i], normal)])
+        
+        '''
+        Step 2
+        Create list of boundary edges
+        '''
+        combinations = []
+        for face in faces:
+            combinations.append([(face[0], face[1]), (face[1], face[2]), (face[2], face[0])])
 
-		edges = [edge for face in combinations for edge in face]
-		boundary_edges = utils.getBoundaryEdges(edges)
+        edges = [edge for face in combinations for edge in face]
+        boundary_edges = utils.getBoundaryEdges(edges)
 
-		'''
-		Step 3
-		For each face, create the extruded face, adding distance * normalVector to its vertices
-		'''
-		for i in range(n_faces):
-			faces.append([v + n_vertices for v in faces[i]])
+        '''
+        Step 3
+        For each face, create the extruded face, adding distance * normalVector to its vertices
+        '''
+        for i in range(n_faces):
+            faces.append([v + n_vertices for v in faces[i]])
 
-		'''
-		Step 4
-		Create sides of extruded shape. For each boundary edge, add the new faces
-		'''
-		for edge in boundary_edges:
-			faces.append([edge[0], edge[1], edge[1] + n_vertices])
-			faces.append([edge[1] + n_vertices, edge[0] + n_vertices, edge[0]])
+        '''
+        Step 4
+        Create sides of extruded shape. For each boundary edge, add the new faces
+        '''
+        for edge in boundary_edges:
+            faces.append([edge[0], edge[1], edge[1] + n_vertices])
+            faces.append([edge[1] + n_vertices, edge[0] + n_vertices, edge[0]])
 
-		'''
-		Step 5
-		Invert normal vector of original faces
-		'''
-		for i in range(n_faces):
-			faces[i].reverse()
+        '''
+        Step 5
+        Invert normal vector of original faces
+        '''
+        for i in range(n_faces):
+            faces[i].reverse()
 
-		new_file = os.path.splitext(file)
-		with open(new_file[0] + '_extruded' + new_file[1], 'w') as f:
-			f.write(''.join(info))
-			f.write(''.join(utils.toString(vertices, 'v')))
-			f.write(''.join(utils.toString(faces, 'f')))
+        new_file = os.path.splitext(file)
+        with open(new_file[0] + '_extruded' + new_file[1], 'w') as f:
+            f.write(''.join(info))
+            f.write(''.join(utils.toString(vertices, 'v')))
+            f.write(''.join(utils.toString(faces, 'f')))
 
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		usage()
-	else:
-		main(sys.argv[1:])
+    if len(sys.argv) < 2:
+        usage()
+    else:
+        main(sys.argv[1:])
--- extrusion/src/utils.py
+++ extrusion/src/utils.py
@@ -2,138 +2,138 @@ import math
 from typing import List, Tuple
 
 def toVertex(line: str) -> List[float]:
-	'''
-	Input: 'v x y z' => Output: [x, y, z]
-	'''
-	coordinates = line.split(' ')[1:]
-	return list(map(float, coordinates))
+    '''
+    Input: 'v x y z' => Output: [x, y, z]
+    '''
+    coordinates = line.split(' ')[1:]
+    return list(map(float, coordinates))
 
 
 def toFace(line: str) -> List[int]:
-	'''
-	Input: 'f v1 v2 v3' => Output: [v1, v2, v3]
-	Input: 'l v1 v2' => Output: [v1, v2]
-	'''
-	vertices = line.split(' ')[1:]
-	return list(map(int, vertices))
+    '''
+    Input: 'f v1 v2 v3' => Output: [v1, v2, v3]
+    Input: 'l v1 v2' => Output: [v1, v2]
+    '''
+    vertices = line.split(' ')[1:]
+    return list(map(int, vertices))
 
 
 def module(vector: List[float]) -> float:
-	'''
-	Input: [x, y, z] => Output: module of such vector
-	'''
-	return math.sqrt(vector[0] ** 2 + vector[1] ** 2 + vector[2] ** 2)
+    '''
+    Input: [x, y, z] => Output: module of such vector
+    '''
+    return math.sqrt(vector[0] ** 2 + vector[1] ** 2 + vector[2] ** 2)
 
 
 def unitary(vector: List[float]) -> List[float]:
-	'''
-	Input: [x, y, z] => Output: Its unitary vector
-	'''
-	mod = module(vector)
-	return [v / mod for v in vector]
+    '''
+    Input: [x, y, z] => Output: Its unitary vector
+    '''
+    mod = module(vector)
+    return [v / mod for v in vector]
 
 
 def toVector(v1: List[float], v2: List[float]) -> List[float]:
-	'''
-	Input: Two vertices [x, y, z] => Output: Vector [x, y, z] composed by such vertices
-	'''
-	return [b - a for (b, a) in zip(v2, v1)]
+    '''
+    Input: Two vertices [x, y, z] => Output: Vector [x, y, z] composed by such vertices
+    '''
+    return [b - a for (b, a) in zip(v2, v1)]
 
 
 def crossProduct(v1: List[float], v2: List[float]) -> List[float]:
-	'''
-	Input: Two vectors [x, y, z] => Output: Vector [x, y, z], result of the cross product
-	'''
-	return [v1[1] * v2[2] - v1[2] * v2[1], - (v1[0] * v2[2] - v1[2] * v2[0]), v1[0] * v2[1] - v1[1] * v2[0]]
+    '''
+    Input: Two vectors [x, y, z] => Output: Vector [x, y, z], result of the cross product
+    '''
+    return [v1[1] * v2[2] - v1[2] * v2[1], - (v1[0] * v2[2] - v1[2] * v2[0]), v1[0] * v2[1] - v1[1] * v2[0]]
 
 
 def normalVector(vertices: List[List[float]], faces: List[List[int]]) -> List[float]:
-	'''
-	Input: Vertices and faces of object => Output: Vector [x, y, z] normal to the surface of the object
-	'''
-	face = faces[0]
-	a = vertices[face[0] - 1]
-	b = vertices[face[1] - 1]
-	c = vertices[face[2] - 1]
+    '''
+    Input: Vertices and faces of object => Output: Vector [x, y, z] normal to the surface of the object
+    '''
+    face = faces[0]
+    a = vertices[face[0] - 1]
+    b = vertices[face[1] - 1]
+    c = vertices[face[2] - 1]
 
-	ab = toVector(a, b)
-	ac = toVector(a, c)
+    ab = toVector(a, b)
+    ac = toVector(a, c)
 
-	normal = crossProduct(ab, ac)
-	return unitary(normal)
+    normal = crossProduct(ab, ac)
+    return unitary(normal)
 
 
 def toString(element: List[List[float]], e_type: str) -> List[str]:
-	'''
-	Input: [v1, v2, v3] => Output: 'f v1 v2 v3'
-	Input: [x, y, z] => Output: 'v x y z'
-	'''
-	s = ''
-	for e in element:
-		e = [round(n, 6) for n in e]
-		e = list(map(str, e))
+    '''
+    Input: [v1, v2, v3] => Output: 'f v1 v2 v3'
+    Input: [x, y, z] => Output: 'v x y z'
+    '''
+    s = ''
+    for e in element:
+        e = [round(n, 6) for n in e]
+        e = list(map(str, e))
 
-		s += e_type + ' '
-		s += ' '.join(e)
-		s += '\n'
+        s += e_type + ' '
+        s += ' '.join(e)
+        s += '\n'
 
-	return s
+    return s
 
 
 def getBoundaryEdges(edges: List[Tuple[int]]) -> List[Tuple[int]]:
-	'''
-	Input: [(v1, v2), (v2, v3) ...] => Output: [(v1, v2), ...] only the boundary edges
-	'''
-	i = 0
-	while i < len(edges):
-		j = i + 1
-		removed = False
-		while j < len(edges):
-			if sorted(edges[i]) == sorted(edges[j]):
-				edges.pop(i)
-				edges.pop(j - 1)
+    '''
+    Input: [(v1, v2), (v2, v3) ...] => Output: [(v1, v2), ...] only the boundary edges
+    '''
+    i = 0
+    while i < len(edges):
+        j = i + 1
+        removed = False
+        while j < len(edges):
+            if sorted(edges[i]) == sorted(edges[j]):
+                edges.pop(i)
+                edges.pop(j - 1)
 
-				removed = True
+                removed = True
 
-			j += 1
-		if not removed:
-			i += 1
+            j += 1
+        if not removed:
+            i += 1
 
-	return edges
+    return edges
 
 
 def multiply(matrix: List[List[float]], vertex: List[float]) -> List[float]:
-	'''
-	Simple matrix multiplication function. Computationally expensive as we increase dimensionality, but, working with 3D, dimensionality is fixed. In further improvements we could optimize this function
-	'''
-	result = [0, 0, 0]
-	for i in range(len(matrix)):
-		for j in range(len(matrix[i])):
-			m = matrix[i][j]
-			v = vertex[j]
-			result[i] += m * v
+    '''
+    Simple matrix multiplication function. Computationally expensive as we increase dimensionality, but, working with 3D, dimensionality is fixed. In further improvements we could optimize this function
+    '''
+    result = [0, 0, 0]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            m = matrix[i][j]
+            v = vertex[j]
+            result[i] += m * v
 
-	return result
+    return result
 
 
 def toRadians(angle: float) -> float:
-	'''
-	Input: degrees => Output: radians
-	'''
-	return angle * math.pi / 180
+    '''
+    Input: degrees => Output: radians
+    '''
+    return angle * math.pi / 180
 
 
 def rotate(vertex: List[float], angle: float, axis: str) -> List[float]:
-	'''
-	Input: [x, y, z] => Output: [x, y, z] with rotated coordinates
-	'''
-	angle = toRadians(angle)
+    '''
+    Input: [x, y, z] => Output: [x, y, z] with rotated coordinates
+    '''
+    angle = toRadians(angle)
 
-	if axis == 'x':
-		matrix = [[1, 0, 0], [0, math.cos(-angle), -math.sin(-angle)], [0, math.sin(-angle), math.cos(-angle)]]
-	elif axis == 'y':
-		matrix = [[math.cos(angle), -math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0 ], [0, 0, 1]]
-	else:
-		matrix = [[math.cos(-angle), 0, math.sin(-angle)], [0, 1, 0], [-math.sin(-angle), 0, math.cos(-angle)]]
+    if axis == 'x':
+        matrix = [[1, 0, 0], [0, math.cos(-angle), -math.sin(-angle)], [0, math.sin(-angle), math.cos(-angle)]]
+    elif axis == 'y':
+        matrix = [[math.cos(angle), -math.sin(angle), 0], [math.sin(angle), math.cos(angle), 0 ], [0, 0, 1]]
+    else:
+        matrix = [[math.cos(-angle), 0, math.sin(-angle)], [0, 1, 0], [-math.sin(-angle), 0, math.cos(-angle)]]
 
-	return multiply(matrix, vertex)
+    return multiply(matrix, vertex)
